3547e3b5066fbf8058f216f6eb7bae64450ef771
Dan Taylor <dt.raelcun@gmail.com>
2014-09-07 03:25:55 -0400

    content of index page has been split into two views index page now looks identical to original

--- js/app.js
+++ js/app.js
@@ -2,43 +2,15 @@ var routerApp = angular.module('mainApp', ['ui.router']);
 
 routerApp.config(function($stateProvider, $urlRouterProvider) {
     
-    // $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/home'); // default state
     
-    // $stateProvider
-        
-    //     // HOME STATES AND NESTED VIEWS ========================================
-    //     .state('home', {
-    //         url: '/home',
-    //         templateUrl: 'views/partial-home.html'
-    //     })
-        
-    //     // nested list with custom controller
-    //     .state('home.list', {
-    //         url: '/list',
-    //         templateUrl: 'views/partial-home-list.html',
-    //         controller: function($scope) {
-    //             $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-    //         }
-    //     })
-        
-    //     // nested list with just some random string data
-    //     .state('home.paragraph', {
-    //         url: '/paragraph',
-    //         template: 'I could sure use a drink right now.'
-    //     })
-        
-    //     // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-    //     .state('about', {
-    //         url: '/about',
-    //         views: {
-    //             '': { templateUrl: 'views/partial-about.html' },
-    //             'columnOne@about': { template: 'Look I am a column!' },
-    //             'columnTwo@about': { 
-    //                 templateUrl: 'views/table-data.html',
-    //                 controller: 'scotchController'
-    //             }
-    //         }
-    //     });
-        
+    $stateProvider
+      .state('home', {
+          url: '/home',
+          views: {
+            'highlighted': { templateUrl: 'views/partial-home-highlighted.html' },
+            'content': { templateUrl: 'views/partial-home.html' }
+          }
+      })
 });
 
