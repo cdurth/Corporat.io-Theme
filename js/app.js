3fda3055f801aa10cc0efc289d419dac7955c203
Matt Levine <mtlevine0@gmail.com>
2014-09-08 02:46:46 +0000

    Added some partials and some route functionality.  Also added .gitignore please update it when necessary

--- js/app.js
+++ js/app.js
@@ -12,5 +12,26 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
             'content': { templateUrl: 'views/partial-home.html' }
           }
       })
+      .state('services',{
+        url: '/services',
+        views: {
+          'highlighted': {templateUrl: 'views/partial-services.html' },
+          'content': { templateUrl: 'views/partial-home.html' }
+        }
+      })
+      .state('portfolio',{
+        url: '/portfolio',
+        views: {
+          'highlighted': {templateUrl: 'views/partial-portfolio.html' },
+          'content': { templateUrl: 'views/partial-home.html' }
+        }
+      })
+      .state('about',{
+        url: '/about',
+        views: {
+          'highlighted': {templateUrl: 'views/partial-about.html' },
+          'content': { templateUrl: 'views/partial-home.html' }
+        }
+      })
 });
 
